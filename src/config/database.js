2a8c833a01927b6bcd18314927fe893e8b9d06b3
Kauã Klassmann <kaua.klassmann661@gmail.com>
2023-10-06 13:07:25 -0300

    again

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const databaseConfig = {
     dialect: 'postgres',
-    host: 'dpg-ckg2i7uct0pc73a0q9f0-a',
+    host: 'dpg-ckg2i7uct0pc73a0q9f0-a:5432',
     username: 'edas',
     password: 'ZfHoXMnWMZTbv6ne8Fa7b4rF4b39ulR8',
     database: 'edas_ifrs_feliz',
