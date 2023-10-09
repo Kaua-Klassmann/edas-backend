1ca6461a158759a36b6384b1af993850b49529a6
Kauã Klassmann <131408936+Kaua-Klassmann@users.noreply.github.com>
2023-10-09 11:30:06 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,7 @@ const databaseConfig = {
     password: 'ZfHoXMnWMZTbv6ne8Fa7b4rF4b39ulR8',
     database: 'edas_ifrs_feliz',
     logging: false,
+    ssl: false,
     timezone: "-03:00",
     define: {
         timestamps: true,
