d1d8c878d5c84ff7b3ed9773e14ac434f61eb62d
Kauã Klassmann <kaua.klassmann661@gmail.com>
2023-10-09 21:55:13 -0300

    database novo

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,10 @@
 const databaseConfig = {
     dialect: 'postgres',
-    host: 'dpg-ckg2i7uct0pc73a0q9f0-a',
+    host: 'postgres://edas:ZfHoXMnWMZTbv6ne8Fa7b4rF4b39ulR8@dpg-ckg2i7uct0pc73a0q9f0-a/edas_ifrs_feliz',
     username: 'edas',
     password: 'ZfHoXMnWMZTbv6ne8Fa7b4rF4b39ulR8',
     database: 'edas_ifrs_feliz',
     logging: false,
-    ssl: false,
     timezone: "-03:00",
     define: {
         timestamps: true,
