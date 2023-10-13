7cc7a47599fbfb8aeab09fd47d5a64c4487333e9
Kauã Klassmann <kaua.klassmann661@gmail.com>
2023-10-13 18:55:18 -0300

    Adição de variaveis de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -4,12 +4,12 @@ const databaseConfig = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    logging: false,
-    timezone: "-03:00",
+    logging: process.env.DB_LOGGING,
+    timezone: process.env.DB_TIMEZONE,
     define: {
-        timestamps: true,
-        underscored: false,
-        freezeTableName: true,
+        timestamps: process.env.DB_TIMESTAMPS,
+        underscored: process.env.DB_UNDERSCORED,
+        freezeTableName: process.env.DB_FREEZETABLENAME,
     },
 };
 
