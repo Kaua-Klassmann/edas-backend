954260192217dab812040ce3dfda6b55d3198b1d
Kauã Klassmann <kaua.klassmann661@gmail.com>
2023-10-19 18:50:33 -0300

    .

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -60,10 +60,10 @@ class UsuarioController {
 
             const code = (await bcrypt.hash(email, 8)).replace(/[^a-zA-Z0-9]/g, "");
 
-            const sql = 'INSERT INTO "Ativacao" ("usuario","codigo") VALUES (?,?,?);';
-            const values = [email, code];
-            await client.query(sql, values);
-
+            await Ativacao.create({
+                "usuario": email,
+                "codigo": code
+            });
           } catch (error) {
             return res.status(400).json({ error: "Algum erro."});
         }
--- src/models/Ativacao.js
+++ src/models/Ativacao.js
@@ -4,6 +4,12 @@ class Ativacao extends Model {
     static init(sequelize) {
         super.init(
             {
+                id: {
+                    type: DataTypes.INTEGER,
+                    autoIncrement: true,
+                    primaryKey: true
+                },
+                
                 usuario: {
                     type: DataTypes.STRING(255),
                 },
