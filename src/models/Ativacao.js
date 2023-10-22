6fea12385c7d8f56f8348b3557050d18ba97d592
Kauã Klassmann <kaua.klassmann661@gmail.com>
2023-10-22 09:12:19 -0300

    .

--- src/models/Ativacao.js
+++ src/models/Ativacao.js
@@ -4,6 +4,11 @@ class Ativacao extends Model {
     static init(sequelize) {
         super.init(
             {
+                id: {
+                    type: DataTypes.INTEGER,
+                    autoIncrement: true,
+                    primaryKey: true
+                },
                 email: {
                     type: DataTypes.STRING,
                 },
