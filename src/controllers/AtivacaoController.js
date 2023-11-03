a8c554ebe568f3f65d7116dfdd7415a1f3b62c17
Kauã Klassmann <kaua.klassmann661@gmail.com>
2023-11-03 19:53:48 -0300

    .

--- src/controllers/AtivacaoController.js
+++ src/controllers/AtivacaoController.js
@@ -36,10 +36,9 @@ class AtivacaoController {
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
             rel="stylesheet" crossorigin="anonymous">
         </head>
-        <body style="heigth: 100vh; background: linear-gradient(45deg, #21d4fd 0%, #b721ff 100%);
-        display: grid; place-items: center;">
-            <main class="rounded-3 p-3 bg-light" style="text-align: center; max-width: 60vw">
-                <h3>Conta com o email '${email}' ativada com sucesso!'</h3>
+        <body style="heigth: 100vh; background: linear-gradient(45deg, #21d4fd 0%, #b721ff 100%); display: grid; place-items: center;">
+            <main class="rounded-3 p-3 bg-light" style="text-align: center; max-width: 60vw; font-size: 30px;">
+                <p>Conta com o email '${email}' ativada com sucesso!'</p>
             </main>
         </body>
         </html>`);
