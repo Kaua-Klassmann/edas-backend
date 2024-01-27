1434e43932bcf500fe5dfc46cfcc66b22858bdbc
Kauã Klassmann <kaua.klassmann661@gmail.com>
2024-01-27 14:54:27 -0300

    .

--- src/controllers/ProvaController.js
+++ src/controllers/ProvaController.js
@@ -3,6 +3,7 @@ import Prova from "../models/Prova.js";
 import * as Yup from 'yup';
 import Turma from "../models/Turma.js";
 import Usuario from "../models/Usuario.js";
+import Disciplina from "../models/Disciplina.js";
 
 
 class ProvaController {
@@ -92,37 +93,49 @@ class ProvaController {
         if(!provas) {
             return res.status(400).json({error: "Prova não encontrada"});
         };
-
-        await provas.forEach(async prova => {
+        
+        for(let i = 0; i < provas.length; i++){
             async function turma() {
-                const id = prova.turma
+                const id = provas[i].turma
                 const turma = await Turma.findOne({
                     where: {
                         id: id
                     }
                 })
                 const { nome } = turma
-                prova.turma = {
+                provas[i].turma = {
                     id,
                     nome
                 }
             }
             async function usuario() {
-                const id = prova.usuario
+                const id = provas[i].usuario
                 const usuario = await Usuario.findOne({
                     where: {
                         id: id
                     }
                 })
                 const { nome } = usuario
-                prova.usuario = {
+                provas[i].usuario = {
                     id,
                     nome
                 }
             }
-            await turma()
-            await usuario()
-        });
+            async function disciplina() {
+                const id = provas[i].disciplina
+                const disciplina = await Disciplina.findOne({
+                    where: {
+                        id: id
+                    }
+                })
+                const { nome } = disciplina
+                provas[i].disciplina = {
+                    id,
+                    nome
+                }
+            }
+            await turma(), await usuario(), await disciplina()
+        }
 
         return res.json(provas);
     };
